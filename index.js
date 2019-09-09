0cd61f8c24d89757add9e98ab4d7361718bd4cfa
elsonlim <elsonlim7@gmail.com>
2019-09-09 15:17:01 +0800

    examples for loop and map

---
+++ index.js
@@ -0,0 +1,49 @@
+const data = require("./pokemonData");
+
+// 1. print out all the english name of all the pokemons
+// try using for(let i =0; i < data.length; i++)
+// for(let item of data)
+// data.forEach
+
+const loop1 = () => {
+  for (let i = data.length - 1; i >= 0; i--) {
+    console.log(data[i].name.english);
+  }
+};
+loop1();
+
+const loop2 = () => {
+  for (let item of data) {
+    console.log(item.name.english);
+  }
+};
+loop2();
+
+const loop3 = () => {
+  data.forEach(item => {
+    console.log(item.name.english);
+  });
+};
+loop3();
+
+// 2. Map
+const markedPokemons1 = data.map(item => {
+  const newItem = {};
+  newItem.id = item.id;
+  newItem.name = item.name.english;
+  return newItem;
+});
+
+const markedPokemons2 = data.map(function(item) {
+  return {
+    id: item.id,
+    name: item.name.english,
+  };
+});
+
+const markedPokemons3 = data.map(item => ({
+  id: item.id,
+  name: item.name.english,
+}));
+
+console.log(markedPokemons1[0]);
